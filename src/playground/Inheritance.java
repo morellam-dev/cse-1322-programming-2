49aa726f0e642a5810431cc3b9840f5174142ace
MozzarellaM <4561733+MozzarellaM@users.noreply.github.com>
2019-09-10 20:15:10 -0400

    Mess around

---
+++ src/playground/Inheritance.java
@@ -0,0 +1,23 @@
+// package playground;
+
+// public class Inheritance {}
+
+// class Dog {
+//     static private int dogCount;
+//     static public int dogCount() {
+//         return dogCount;
+//     }
+
+//     private boolean alive;
+//     public Dog() {
+//         dogCount++;
+//         this.alive = true;
+//     }
+//     public void destroy() {
+//         if (alive = true) {
+//             System.out.println("You monster!");
+//             this.alive = false;
+//             dogCount--;
+//         }
+//     }
+// }
