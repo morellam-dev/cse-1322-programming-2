f01dd206f2eff2b727874eb5e57204e0bf5cbe58
MozzarellaM <4561733+MozzarellaM@users.noreply.github.com>
2019-09-25 14:07:44 -0400

    Lab 3A: Sort message literals

--- src/module_3/lab_3a_swing/ButtonGUI.java
+++ src/module_3/lab_3a_swing/ButtonGUI.java
@@ -4,43 +4,43 @@ import java.awt.*;
 import java.awt.event.*;
 
 /**
- * A simple component which displays buttons, and shows output when a button is pressed.
- */
+* A simple component which displays buttons, and shows output when a button is pressed.
+*/
 public class ButtonGUI extends Panel {
     private static final long serialVersionUID = 2188026364856631809L;
     
-	private TextField textField;
+    private TextField textField;
     private Panel buttonPanel;
     private String[] buttonNames;
-
+    
     /**
-     * A simple component which displays six buttons, and shows output when a button is
-     * pressed.
-     */
+    * A simple component which displays six buttons, and shows output when a button is
+    * pressed.
+    */
     public ButtonGUI() {
         // Default behavior: six numbered buttons
         this(new String[]{"Button #1", "Button #2", "Button #3", 
-            "Button #4", "Button #5", "Button #6"});
+        "Button #4", "Button #5", "Button #6"});
     }
-
+    
     /**
-     * Create a ButtonGUI with custom button names
-     * 
-     * @param buttonNames The names of each button to display on the panel.
-     * @param messages    A list of formatting strings like {@code "You pressed %s"},
-     *                    which will be displayed at random when a button is pressed.
-     */
+    * Create a ButtonGUI with custom button names
+    * 
+    * @param buttonNames The names of each button to display on the panel.
+    * @param messages    A list of formatting strings like {@code "You pressed %s"},
+    *                    which will be displayed at random when a button is pressed.
+    */
     public ButtonGUI(String[] buttonNames) {
         super(new BorderLayout(5, 5)); // set layout manager
-
+        
         this.buttonNames = buttonNames;
-
+        
         // Text display component
         textField = new TextField(40);
         textField.setText("Welcome! Press a button and see what happens!");
         textField.setEditable(false);
         add(textField, BorderLayout.PAGE_START);
-
+        
         // Button component: a 3-column grid
         buttonPanel = new Panel(new GridLayout(0, 3));
         // Generate six buttons
@@ -56,23 +56,23 @@ public class ButtonGUI extends Panel {
         add(buttonPanel, BorderLayout.PAGE_END);
     }
     /**
-     * Given the name of the button that was just pressed, return a random message.
-     * @param buttonName
-     * @return A formatted string including the button name.
-     */
+    * Given the name of the button that was just pressed, return a random message.
+    * @param buttonName
+    * @return A formatted string including the button name.
+    */
     public void showButtonMessage(String buttonName) {
         String[] messages = { 
-            "Blam! You pressed %s!", 
-            "Wowie! %s was pressed!",
             "Bazinga! You pressed the heck out of %s!", 
-            "Wowie! You just pressed %s!", 
-            "Outstanding! That was %s!",
-            "Kapow! Nice work on pressing %s!", 
-            "Whoa mama! Did you just press %s?", 
+            "Blam! You pressed %s!", 
             "Hachi machi, you pressed %s.",
-            "Press %s again. See what happens.", 
             "Huh. Somebody just pushed %s.",
-            "何!? %s が押されました!" 
+            "Kapow! Nice work on pressing %s!", 
+            "Outstanding! That was %s!",
+            "Press %s again. See what happens.", 
+            "Whoa mama! Did you just press %s?", 
+            "Wowie! %s was pressed!",
+            "Wowie! You just pressed %s!", 
+            "何!? %s が押されました!"
         };
         // Get random message format.
         String msgFormat = messages[(int) (Math.random() * messages.length)];
