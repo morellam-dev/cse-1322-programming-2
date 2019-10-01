d3b72ff7f21d31ead0230cbd6f9f4d5bbab775ca
MozzarellaM <4561733+MozzarellaM@users.noreply.github.com>
2019-10-01 15:22:37 -0400

    Lab 4b: better recursive implementation

--- src/module_4/lab_4b/PalindromeChecker.java
+++ src/module_4/lab_4b/PalindromeChecker.java
@@ -7,43 +7,30 @@ package module_4.lab_4b;
  */
 public class PalindromeChecker {
     /**
-     * Detects palindromic words using a recursive method. A palindrome is defined as any
+     * Detects palindromic words and phrases using a recursive method. A palindrome is defined as any
      * string, which, when all its characters are reversed, produces the same
-     * string. As such, both {@code "a"} and {@code ""} are considered valid palindromes.
+     * string. As such, {@code "ada"}, {@code "a"}, and {@code ""} are all considered valid palindromes.
+     * All non-alphanumeric characters are ignored, so {@code "Madam, I'm Adam"} 
+     * is converted to {code "madamimadam"}, and considered a valid palindrome.
      * 
      * @param str A string which may or may not contain a palindrome
      * @return Whether or not the input is a valid palindrome
      */
-    public static boolean isPalindrome(String word) {
-        // Exit case 1: the substring is less than 2 characters.
-        if (word.length() < 2) {
-            return true;
-        }
-        
-        char firstChar = word.charAt(0);
-        char lastChar = word.charAt(word.length() - 1);
-
-        /* Recursive case: If the first and last characters match, 
-        then recurse on a substring which excludes the outermost characters. */
-        if (firstChar == lastChar) {
-            String internal = word.substring(1, word.length() - 1); // "madam" => "ada" => "d"
-            return isPalindrome(internal);
-        }
-        // Exit case 2: the first and last characters do not match.
-        return false;
+    public static boolean isPalindrome(String sentence) {
+        String str = sentence.toLowerCase().trim().replaceAll("[^a-zA-Z0-9]", "");
+        return isPalindromeHelper(str, 0, str.length() - 1);
     }
 
-    /**
-     * Performs the same functionality as `isPalindrome`, but mutates the input string to 
-     * remove all spaces and punctuation. This allows more complicated palindromes like
-     * "No 'x' in Nixon" to return true.
-     * @param str A string to test.
-     * @return Whether or not the string is a palindrome
-     */
-    public static boolean isPalindromeClean(String sentence) {
-        // Remove all non-alphanumeric characters, trim and make lowercase.
-        String simplified = sentence.toLowerCase().trim().replaceAll("[^a-zA-Z0-9]", "");
-        return isPalindrome(simplified);
+    // Recursive method
+    private static boolean isPalindromeHelper(String str, int indexBegin, int indexEnd) {
+        // End case 1: the characters at the two indexes are not equal.
+        if (str.charAt(indexBegin) != str.charAt(indexEnd))
+            return false;
+        // End case 2: the string has reached the middle, and all checks so far have
+        // returned true.
+        if (indexBegin >= indexEnd)
+            return true;
+        return isPalindromeHelper(str, ++indexBegin, --indexEnd);
     }
 
     public static void main(String[] args) {
@@ -52,13 +39,14 @@ public class PalindromeChecker {
                 "Woh! OwO OwO. How?", 
                 "No 'x' in Nixon?", 
                 "Able was I, ere I saw Elba",
-                "Hello world", 
+                "Was it a car or a cat I saw?",
                 "aloha ahola", 
+                "Hello world", 
                 "bananab",
                 "This sentence is not a palindrome.", 
-                "Was it a car or a cat I saw?"};
+                };
         for (String str : tests) {
-            System.out.println("isPalindromeClean(\"" + str + "\") => " + isPalindromeClean(str));
+            System.out.println("isPalindrome(\"" + str + "\") => " + isPalindrome(str));
         }
     }
 }
