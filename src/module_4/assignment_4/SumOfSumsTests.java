26d3b9a54fd5025c8b28d04366fddf3e8097abb8
MozzarellaM <4561733+MozzarellaM@users.noreply.github.com>
2019-10-18 19:24:21 -0400

    Assignment 4 - Add demo output

--- src/module_4/assignment_4/SumOfSumsTests.java
+++ src/module_4/assignment_4/SumOfSumsTests.java
@@ -3,17 +3,17 @@ package module_4.assignment_4;
 import java.util.Arrays;
 
 /**
- * Performs manual unit tests on the SumOfSums class to ensure output is as expected.
+ * Performs unit tests on the {@code sumOfSums()} method to ensure output is as expected.
  * @author M Morella
  */
 public class SumOfSumsTests {
     private static void testSumOfSums(int[] testArray, int expectedValue) {
-        int sum = SumOfSums.sumOfSums(testArray);
-        boolean testPassed = (sum == expectedValue);
+        int actualValue = SumOfSums.sumOfSums(testArray);
+        boolean testPassed = (actualValue == expectedValue);
         
-        System.out.printf("TEST: sumOfSums(%1$s) -> %2$d", Arrays.toString(testArray), sum);
+        System.out.printf("TEST: sumOfSums(%1$s) -> %2$d", Arrays.toString(testArray), actualValue);
         System.out.println();
-        System.out.printf(" - expected %1$d - returned %2$d", expectedValue, sum);
+        System.out.printf(" - expected %1$d - returned %2$d", expectedValue, actualValue);
         System.out.print(" – ");
         System.out.println(testPassed ? "👍  Test passed!" : "❌  Test failed!");
     }
@@ -26,3 +26,18 @@ public class SumOfSumsTests {
         testSumOfSums(new int[] { 3, 2, 1 }, 10);
     }
 }
+/*
+CONSOLE OUTPUT:
+
+TEST: sumOfSums([5, 18, 4, 7, 11]) -> 136
+ - expected 136 - returned 136 – 👍  Test passed!
+TEST: sumOfSums([50, 19, 1, -49, 3, 99, 3, 21, 63]) -> 1260
+ - expected 1260 - returned 1260 – 👍  Test passed!
+TEST: sumOfSums([]) -> 0
+ - expected 0 - returned 0 – 👍  Test passed!
+TEST: sumOfSums([1]) -> 1
+ - expected 1 - returned 1 – 👍  Test passed!
+TEST: sumOfSums([3, 2, 1]) -> 10
+ - expected 10 - returned 10 – 👍  Test passed!
+
+ */
