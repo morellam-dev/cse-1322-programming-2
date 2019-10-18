59da6e3dee8d1b45af920848fff6bde92363620f
MozzarellaM <4561733+MozzarellaM@users.noreply.github.com>
2019-10-18 19:24:33 -0400

    Assignment 4 - Add Javadoc to StringReverser class

--- src/module_4/assignment_4/StringReverser.java
+++ src/module_4/assignment_4/StringReverser.java
@@ -1,11 +1,15 @@
 package module_4.assignment_4;
 
 /**
- * StringReverser
+ * A simple demonstration of recursive methods, which prints 
+ * a string, then prints a string in reverse.
  * @author M Morella
  */
 public class StringReverser {
-
+    /**
+     * Prints a string to the console, character by character.
+     * @param str A string
+     */
     public static void printString(String str) {
         if (str.length() == 0) {
             System.out.print('\n');
@@ -14,7 +18,11 @@ public class StringReverser {
             printString(str.substring(1));
         }
     }
-
+    /**
+     * Prints a string to the console in reverse. For instance, {@code "Hello world!"} 
+     * results in printing {@code "!dlrow olleH"}
+     * @param str
+     */
     public static void printStringReverse(String str) {
         if (str.length() == 0) {
             System.out.print('\n');
@@ -24,10 +32,17 @@ public class StringReverser {
             printStringReverse(str.substring(0, str.length() - 1));
         }
     }
+    /** 
+     * Demonstrates the "Hello world!" example shown in the assignemnt instructions.
+     * Outputs to the console, <code>"Hello world!"</code> and <code>"!dlrow olleH"</code>
+     */
     public static void main(String[] args) {
         System.out.println();
         String demo = "Hello world!";
-        printString(demo);
-        printStringReverse(demo);
+        printString(demo); 
+        // -> "Hello world!"
+        printStringReverse(demo); 
+        // -> "!dlrow olleH"
+        
     }
 }
