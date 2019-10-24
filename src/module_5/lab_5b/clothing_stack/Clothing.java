a0f598d5c669315699113f5e28c31f87c8988013
MozzarellaM <4561733+MozzarellaM@users.noreply.github.com>
2019-10-24 13:07:13 -0400

    Lab 5b - Rewrite Clothing data class

--- src/module_5/lab_5b/clothing_stack/Clothing.java
+++ src/module_5/lab_5b/clothing_stack/Clothing.java
@@ -1,53 +1,48 @@
 package module_5.lab_5b.clothing_stack;
 
 /**
- * A data-type referring to a single article of clothing
- * @author M. Morella 
+ * Clothing
  */
 public class Clothing {
-    final private String name;
-    final private String color;
-    final private boolean highTempWashable;
- 
-    /**
-     * Initializes a newly created Clothing object
-     * @param name The name of the clothing
-     * @param color The color of the clothing
-     * @param highTemp Whether or not the clothes can be washed at high temperatures
-     */
-    public Clothing(String name, String color, boolean highTempWashable) {
+    private final String name;
+    private final String color;
+    private final boolean machineWashable;
+
+    /** Create a new article of Clothing 
+     * @param color           The color of the clothing (ex. "red")
+     * @param name            The name of the clothing (ex. "shirt")
+     * @param machineWashable Whether the clothing is machine washable
+    */
+    public Clothing(String color, String name, boolean machineWashable) {
         this.name = name;
         this.color = color;
-        this.highTempWashable = highTempWashable;
+        this.machineWashable = machineWashable;
     }
-
-    /** @return The name of the clothing */
+    /** @return the name of the clothing */
     public String getName() {
         return this.name;
     }
 
-    /** @return The color of the clothing */
+    /** @return the color of the clothing */
     public String getColor() {
         return this.color;
     }
 
-    /** @return Whether or not the clothes can be washed at high temperatures */
-    public boolean isHighTempWashable() {
-        return this.highTempWashable;
+    /** @return whether or not the clothing is machine-washable */
+    public boolean getMachineWashable() {
+        return this.machineWashable;
+    }
+    /** @return whether or not the clothing is machine-washable */
+    public boolean isMachineWashable() {
+        return getMachineWashable();
     }
 
 
     @Override
     public String toString() {
-        return "{" +
-            " name='" + getName() + "'" +
-            ", color='" + getColor() + "'" +
-            ", highTempWashable='" + isHighTempWashable() + "'" +
-            "}";
+        return color + " " + name 
+            + (machineWashable ? " (machine-washable)" : "");
     }
 
-    public String toSimpleString() {
-        String washable = highTempWashable? " (high-temp washable)" : "";
-        return color + " " + name + washable;
-    }
+    
 }
