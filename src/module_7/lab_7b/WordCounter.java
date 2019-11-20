1f52099e78e4ada6cb0f876cd45d1a225d3fc48c
MozzarellaM <4561733+MozzarellaM@users.noreply.github.com>
2019-11-20 13:07:22 -0500

    Lab 7a - Rewrite WordCounter.java

--- src/module_7/lab_7b/WordCounter.java
+++ src/module_7/lab_7b/WordCounter.java
@@ -1,89 +1,101 @@
 package module_7.lab_7b;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.Arrays;
+import java.util.regex.*;
 
 /**
- * WordCounter
+ * WordCount
  */
-public class WordCounter extends Thread {
-    private final String word;
-    private final String[] strings;
-    private final int startIndex;
-    private final int endIndex;
-
-    private int count;
-
-    public WordCounter(String[] strings, String word, int startIndex, int endIndex) {
-        this.word = word;
-        this.strings = strings;
-        this.startIndex = startIndex;
-        this.endIndex = Math.min(endIndex, strings.length);
-    }
-
-
-    // Thread Activity
-
-    @Override
-    public void run() {
-        count = 0;
-        for (int i = startIndex; i < endIndex ; i++) {
-            count += count(strings[i], word);
-        }
-    }
-
-    private static int count(String str, String word) {
+public class WordCounter {
+    /**
+     * Using a regular expression, count every occurence of a word. For example,
+     * {@code "the."} is be counted, but {@code "then"} or {@code "at he"} is not
+     * @param text The body of text to search through
+     * @param word The word or phrase to search for
+     * @return
+     */
+    public static int wordCount(String text, String word) {
+        Pattern pattern = Pattern.compile(word, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(text);
+    
         int count = 0;
-        int searchIndex = str.toLowerCase().indexOf(word.toLowerCase());
-        while (searchIndex > -1) {
+        int i = 0;
+        while (matcher.find(i)) {
+            i = matcher.end();
             count++;
-            str = str.substring(searchIndex + 1);
-            searchIndex = str.toLowerCase().indexOf(word.toLowerCase());
         }
         return count;
     }
 
-    public int getCount() {
-        return count;
+    private static String[] splitStringIntoChunks(String text, int count) {
+        String[] lines = text.split("\n");
+        int sliceLength = (int)Math.ceil(lines.length / count);
+
+        String[] chunks = new String[count];
+        for (int i = 0; i < count; i++) {
+            int from = i * sliceLength;
+            int to = Math.min((i + 1) * sliceLength, lines.length);
+            String[] sub = Arrays.copyOfRange(lines, from, to);
+            chunks[i] = String.join("\n", sub);
+        }
+        return chunks;
     }
 
-
-
-    public static int wordCount(String fullText, String word) {
-        String[] strings = fullText.split("\n");
-        return wordCount(strings, word);
+    private static String readStringFromFile(String path) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+            String text = "";
+            while (reader.ready()) {
+                text += reader.readLine() + "\n";
+            }
+            return text;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
-    public static int wordCount(String[] strings, String word) {
-        final int THREAD_COUNT = 4;
+    public static int wordCountParallel(String str, String word) {
+        final int THREAD_COUNT = Runtime.getRuntime().availableProcessors();
+        Thread[] threads = new Thread[THREAD_COUNT];
+        int[] results = new int[THREAD_COUNT];
+        String[] chunks = splitStringIntoChunks(str, THREAD_COUNT);
 
-        WordCounter[] threads = new WordCounter[THREAD_COUNT];
-        int sliceLength = (int)Math.ceil(strings.length / THREAD_COUNT);
-        for (int i = 0; i < 4; i++) {
-            WordCounter t = new WordCounter(strings, word, i * sliceLength, (i + 1) * sliceLength);
+        for (int i = 0; i < THREAD_COUNT; i++) {
+            final int index = i; // Lambda expression requires final variables
+            Thread t = new Thread(() -> results[index] = wordCount(chunks[index], word));
             threads[i] = t;
             t.start();
         }
-        int count = 0;
-        for (WordCounter t : threads) {
-            try {
+        try {
+            for (Thread t : threads) {
                 t.join();
-                count += t.getCount();
-            } catch (InterruptedException e) {}
-        }
-        return count;
-    }
-    public static void main(String[] args) {
-        try (BufferedReader reader = new BufferedReader(new FileReader("./alice.txt"))) {
-            String fullText = "";
-            while (reader.ready()) {
-                fullText += reader.readLine() + "\n";
             }
-            int wordCount = WordCounter.wordCount(fullText, "the");
-            System.out.println(wordCount);
-        } catch (IOException e) {
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        int totalCount = 0;
+        for (int partialCount : results) {
+            totalCount += partialCount;
+        }
+        return totalCount;
+    }
+
+    public static void main(String[] args) {
+        String file = "alice.txt";
+        String fullText = readStringFromFile(file);
+        // System.out.println(alice);
+
+        long serialDuration = System.currentTimeMillis();
+        int serialCount = wordCount(fullText, "the");
+        serialDuration = System.currentTimeMillis() - serialDuration;
+        System.out.println("=== SERIAL COUNT ===");
+        System.out.printf("%s has %d occurences of \"the\". (took %,d ms)\n", file, serialCount, serialDuration);
+
+        long parallelDuration = System.currentTimeMillis();
+        int parallelCount = wordCount(fullText, "the");
+        parallelDuration = System.currentTimeMillis() - parallelDuration;
+        System.out.printf("=== PARALLEL COUNT (%d THREADS) ===\n", Runtime.getRuntime().availableProcessors());
+        System.out.printf("%s has %d occurences of \"the\". (took %,d ms)\n", file, parallelCount, parallelDuration);
     }
 }
